698e65e33723e327ffe93840b094330246ae3d9f
Crazy4Redstone <mcpe1398@gmail.com>
2014-03-19 14:24:05 -0500

    Update s.js

--- s.js
+++ s.js
@@ -29,4 +29,37 @@ Level.destroyBlock(getPlayerX(), getPlayerY()+2, getPlayerZ()-1);
 Level.destroyBlock(getPlayerX()-1, getPlayerY()+2, getPlayerZ()); 
 Level.destroyBlock(getPlayerX()-1, getPlayerY()+2, getPlayerZ()-1); 
 } 
-} 
+if (getTile (getPlayerX (),getPlayerY ()-1.7,getPlayerZ ()) == 78)
+{
+setTile(getPlayerX()+1, getPlayerY()+2, getPlayerZ(), 78); 
+setTile(getPlayerX(), getPlayerY()+2, getPlayerZ()+1, 78); 
+setTile(getPlayerX()+1, getPlayerY()+2, getPlayerZ()+1, 78); 
+setTile(getPlayerX(), getPlayerY()+2, getPlayerZ()-1, 78); 
+setTile(getPlayerX()-1, getPlayerY()+2, getPlayerZ(), 78); 
+setTile(getPlayerX()-1, getPlayerY()+2, getPlayerZ()-1, 78); 
+  
+Level.destroyBlock(getPlayerX()+1, getPlayerY()+2, getPlayerZ()); 
+Level.destroyBlock(getPlayerX(), getPlayerY()+2, getPlayerZ()+1); 
+Level.destroyBlock(getPlayerX()+1, getPlayerY()+2, getPlayerZ()+1); 
+Level.destroyBlock(getPlayerX(), getPlayerY()+2, getPlayerZ()-1); 
+Level.destroyBlock(getPlayerX()-1, getPlayerY()+2, getPlayerZ()); 
+Level.destroyBlock(getPlayerX()-1, getPlayerY()+2, getPlayerZ()-1); 
+}
+else
+{
+  setTile(getPlayerX()+1, getPlayerY()+2, getPlayerZ(), 9); 
+setTile(getPlayerX(), getPlayerY()+2, getPlayerZ()+1, 9); 
+setTile(getPlayerX()+1, getPlayerY()+2, getPlayerZ()+1, 9); 
+setTile(getPlayerX(), getPlayerY()+2, getPlayerZ()-1, 9); 
+setTile(getPlayerX()-1, getPlayerY()+2, getPlayerZ(), 9); 
+setTile(getPlayerX()-1, getPlayerY()+2, getPlayerZ()-1, 9); 
+  
+Level.destroyBlock(getPlayerX()+1, getPlayerY()+2, getPlayerZ()); 
+Level.destroyBlock(getPlayerX(), getPlayerY()+2, getPlayerZ()+1); 
+Level.destroyBlock(getPlayerX()+1, getPlayerY()+2, getPlayerZ()+1); 
+Level.destroyBlock(getPlayerX(), getPlayerY()+2, getPlayerZ()-1); 
+Level.destroyBlock(getPlayerX()-1, getPlayerY()+2, getPlayerZ()); 
+Level.destroyBlock(getPlayerX()-1, getPlayerY()+2, getPlayerZ()-1);
+}
+}
+
