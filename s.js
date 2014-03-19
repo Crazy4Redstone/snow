b1a3657c55ce58e764fb13c9d4f24f293bc683be
Crazy4Redstone <mcpe1398@gmail.com>
2014-03-19 14:15:40 -0500

    Create s.js

---
+++ s.js
@@ -0,0 +1,32 @@
+var rain=0; 
+function useItem(x, y, z, itemId, blockId, side) 
+{ 
+if(blockId==79&&rain==0) 
+{ 
+rain=1; 
+} 
+else if(blockId&&rain==1) 
+{ 
+rain=0; 
+} 
+} 
+  
+function modTick() 
+{ 
+if (rain==1) 
+{ 
+setTile(getPlayerX()+1, getPlayerY()+2, getPlayerZ(), 9); 
+setTile(getPlayerX(), getPlayerY()+2, getPlayerZ()+1, 9); 
+setTile(getPlayerX()+1, getPlayerY()+2, getPlayerZ()+1, 9); 
+setTile(getPlayerX(), getPlayerY()+2, getPlayerZ()-1, 9); 
+setTile(getPlayerX()-1, getPlayerY()+2, getPlayerZ(), 9); 
+setTile(getPlayerX()-1, getPlayerY()+2, getPlayerZ()-1, 9); 
+  
+Level.destroyBlock(getPlayerX()+1, getPlayerY()+2, getPlayerZ()); 
+Level.destroyBlock(getPlayerX(), getPlayerY()+2, getPlayerZ()+1); 
+Level.destroyBlock(getPlayerX()+1, getPlayerY()+2, getPlayerZ()+1); 
+Level.destroyBlock(getPlayerX(), getPlayerY()+2, getPlayerZ()-1); 
+Level.destroyBlock(getPlayerX()-1, getPlayerY()+2, getPlayerZ()); 
+Level.destroyBlock(getPlayerX()-1, getPlayerY()+2, getPlayerZ()-1); 
+} 
+} 
